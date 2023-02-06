55f49de163b575e875ff7bfb7a56d76027cca835
adamtefft <adam_tefft1@baylor.edu>
2023-02-06 17:47:03 -0600

    updated the file

--- earthquakes.py
+++ earthquakes.py
@@ -6,9 +6,9 @@ NOTE: No hard-coding allowed except for keys for the dictionaries
 
 1) print out the number of earthquakes
 
-2) iterate through the dictionary and extract the location, magnitude, 
+2) iterate through the dictionary and extract the location, magnitude,
    longitude and latitude of the location and put it in a new
-   dictionary, name it 'eq_dict'. We are only interested in earthquakes that have a 
+   dictionary, name it 'eq_dict'. We are only interested in earthquakes that have a
    magnitude > 6. Print out the new dictionary.
 
 3) using the eq_dict dictionary, print out the information as shown below (first three shown):
@@ -36,24 +36,43 @@ Latitude: 14.7628
 import json
 
 # PROBLEM 1
-print("This is the solution for problem 1")
 infile = open('eq_data.json', 'r')
+
 earthquakes = json.load(infile)
 
+print("----------------------------")
+print("This is the solution for problem 1")
+print("\n")
+print("Number of Earthquakes:", len(earthquakes["features"]))
+
 # I am thinking that the "type" is an indicator of how many earthquakes there are, so we would need to count that
-print(type(earthquakes))
 
-print(len(earthquakes))
+print("Number of Earthquakes:", earthquakes["metadata"]["count"])
+print("\n")
 print("This is the end of problem 1")
-
+print("----------------------------")
 mag = ("The magnitude of the earthquake, this will be a function that drills down the dictionaries to finally find the magnitude")
 
 
 # PROBLEM 2
-if mag in ["mag"] > 6:
-    for i in earthquakes:
-     # location = earthquake["metadata"]["features"]["properties"]
-        location = earthquakes[i]["type"]["metadata"]["features"]["properties"]["mag"]
-        magnitude = earthquakes[i]["type"]["metadata"]["features"]["properties"]["mag"]
-        print(location)
-        print(magnitude)
+
+eq_dict = {}
+
+print("This is the solution for problem 2")
+print("\n")
+for i in earthquakes["features"]:
+    if i["properties"]["mag"] > 6:
+        # location = earthquake["metadata"]["features"]["properties"]
+        location = i["properties"]["place"]
+        magnitude = i["properties"]["mag"]
+        longitude = i["geometry"]["coordinates"][0]
+        latitude = i["geometry"]["coordinates"][1]
+        print("Location:", location)
+        print("Magnitude:", magnitude)
+        print("Longitude:", longitude)
+        print("Latitude:", latitude)
+        print("\n\n")
+for i in eq_dict:
+    print()
+print("This is the end of problem 2")
+print("----------------------------")
