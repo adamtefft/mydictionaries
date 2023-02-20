7ce9e18ad762d80d5f61345b91ca12885456ae6a
adamtefft <adam_tefft1@baylor.edu>
2023-02-20 14:34:14 -0600

    done

--- 7_schools_report.py
+++ 7_schools_report.py
@@ -23,6 +23,18 @@ import json
 infile = open('school_data.json', 'r')
 
 schools = json.load(infile)
+
+conference_schools = [372, 108, 107, 130]
+
+for i in schools:
+    if i["NCAA"]["NAIA conference number football (IC2020)"] in conference_schools:
+        if i["Graduation rate  women (DRVGR2020)"] > 80:
+            if i["Total price for in-state students living off campus (not with family)  2020-21 (DRVIC2020)"] > 50000:
+                print(i["instnm"])
+
+
+'''
+
 conference_schools = [372, 108, 107, 130]
 # these numbers are based off of the list in the instructions, it identifies what conference the school is in
 
@@ -32,22 +44,11 @@ print(type(schools))
 print(len(schools))
 
 
-'''
-this is my work
-
-for i in schools:
-    graduation_rate = schools[i]["Graduation rate  women (DRVGR2020)"]
-    if graduation_rate <= 80:
-        print(f'{graduation_rate}')
-
-for i in schools:
-    total_price = schools[i][
-        "Total price for in-state students living off campus (not with family)  2020-21 (DRVIC2020)"]
-    if total_price >= 50000:
-        print(f'{total_price}')
 
 
-'''
+
+
+
 # this is what the answer was in class
 
 
@@ -63,19 +64,4 @@ for school in schools:
                       school["Total price for in-state students living off campus (not with family)  2020-21 (DRVIC2020)"])
                 print("\n")
 
-
-'''
-for school in schools:
-    if school['NCAA']["NAIA conference number football (IC2020)"] in conference_schools:
-        if school["Graduation rate  women (DRVGR2020)"] > 80:
-            print("School:", school["instnm"])
-            print("Graduation Rate for Women:",
-                  school["Graduation rate  women (DRVGR2020)"])
-            print("\n")
-
-for school in schools:
-    if school['NCAA']["NAIA conference number football (IC2020)"] in conference_schools:
-        if school["Total price for in-state students living off campus (not with family)  2020-21 (DRVIC2020)"] > 50000:
-            print("In State Tuition:",
-                  school["Total price for in-state students living off campus (not with family)  2020-21 (DRVIC2020)"])
 '''
